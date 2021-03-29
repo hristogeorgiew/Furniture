a6ea4f7255f73913a0e1d3064386fe2c94133004
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-03-29 21:18:40 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import { Switch, Route} from 'react-router-dom';
-import HomePage from './components/Home/HomePage';
+import CreatePage from './components/Create/CreatePage';
+import ProfilePage from './components/Profile/ProfilePage';
+import DetailsPage from './components/Details/DetailsPage';
+import LoginPage from './components/Auth/LoginPage';
+
 
 function App() {
   return (
@@ -7,6 +11,10 @@ function App() {
         <h1>start</h1>
         <Switch>
             <Route exact path='/' component={HomePage} />
+            <Route exact path='/create' component={CreatePage} />
+            <Route exact path='/profile' component={ProfilePage} />
+            <Route exact path='/details/:id' component={DetailsPage} />
+            <Route exact path='/login' component={LoginPage} />
         </Switch>
     </div>
   );
