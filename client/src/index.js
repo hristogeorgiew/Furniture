02890cafa42c92cb1bc804e8fa62536e4d00832a
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-03-29 20:46:39 +0300

    Add style

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './style/bootstrap.min.css';
+import './style/site.css';
 
 ReactDOM.render(
   <React.StrictMode>
