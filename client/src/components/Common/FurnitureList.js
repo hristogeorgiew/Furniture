34f67530406136066ca091d2024d1945ee7e4b84
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-03-30 21:04:05 +0300

    Add Furniture Card

--- client/src/components/Common/FurnitureList.js
+++ client/src/components/Common/FurnitureList.js
@@ -1,58 +1,15 @@
 import React, { Component } from 'react';
+import FurnitureCard from './FurnitureCard';
 
 
 export default class FurnitureList extends Component {
     render(){
         return(
             <div className="row space-top">
-                <div className="col-md-4">
-                    <div className="card text-white bg-primary">
-                        <div className="card-body">
-                            <blockquote className="card-blockquote">
-                                <img src="https://www.ikea.com/mx/en/images/products/uppland-2-seat-sofa-totebo-light-beige__0818550_pe774481_s5.jpg?f=s" />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                <footer>Someone famous in
-                                    <cite title="Source Title">Source Title</cite>
-                                </footer>
-                                <div className="pull-right">
-                                    <a href="details.html" class="btn btn-info">Details</a>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-white bg-primary">
-                        <div className="card-body">
-                            <blockquote className="card-blockquote">
-                                <img src="https://www.ikea.com/mx/en/images/products/uppland-2-seat-sofa-totebo-light-beige__0818550_pe774481_s5.jpg?f=s" />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                <footer>Someone famous in
-                                    <cite title="Source Title">Source Title</cite>
-                                </footer>
-                                <div className="pull-right">
-                                    <a href="details.html" class="btn btn-info">Details</a>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
-                <div className="col-md-4">
-                    <div className="card text-white bg-primary">
-                        <div className="card-body">
-                            <blockquote className="card-blockquote">
-                                <img src="https://www.ikea.com/mx/en/images/products/uppland-2-seat-sofa-totebo-light-beige__0818550_pe774481_s5.jpg?f=s" />
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.</p>
-                                <footer>Someone famous in
-                                    <cite title="Source Title">Source Title</cite>
-                                </footer>
-                                <div className="pull-right">
-                                    <a href="details.html" class="btn btn-info">Details</a>
-                                </div>
-                            </blockquote>
-                        </div>
-                    </div>
-                </div>
+                <FurnitureCard />
+                <FurnitureCard />
+                <FurnitureCard />
+                
             </div>
         )
     }
