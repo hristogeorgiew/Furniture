701e7ddf822c485073e2f24240d3f9a521f8dab5
Hristo <h.georgiew93@gmail.com>
2021-04-02 11:34:11 +0300

    Create furnitureActions.js

---
+++ client/src/actions/furnitureActions.js
@@ -0,0 +1,24 @@
+import { AJAX_BEGIN, AJAX_ERROR, FETCH_PAGE_SUCCESS, FETCH_DETAILS_SUCCESS, FETCH_SEARCH_SUCCESS } from './actionTypes';
+import { fetchPage, fetchDetails, fetchSearchPage } from '../api/remote';
+
+function fetchSuccess(data) {
+    return {
+        type: FETCH_PAGE_SUCCESS,
+        data
+    }
+}
+
+export function fetchPageAction(page) {
+    return async (dispatch) => {
+        dispatch({ type: AJAX_BEGIN });
+        try {
+           const data = await fetchPage(page);
+           dispatch(fetchSuccess(data))
+        } catch (error) {
+            dispatch({ 
+                type: AJAX_ERROR,
+                error
+            });
+        }
+    };
+}
