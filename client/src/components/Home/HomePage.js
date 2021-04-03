6e231e65e143cb1f13250b126ecb0192d971fc41
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-04-03 10:38:17 +0300

    add search functionality

--- client/src/components/Home/HomePage.js
+++ client/src/components/Home/HomePage.js
@@ -7,13 +7,39 @@ import { connect } from 'react-redux';
 
 class HomePage extends Component {
 
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            query: ''
+        }
+
+        this.onChange = this.onChange.bind(this);
+    }
+
+    onChange(e) {
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    onSubmit(e) {
+        e.preventDefault();
+        this.props.fetchSerach(this.state.query, 1);
+    }
+
     componentWillMount() {
         this.props.fetchPage(1);
     }
 
+
     render() {
 
-        const { furniture } = this.props;
+        let furniture = this.props.furniture;
+        const page = this.props.match.params.page || 1;
+        if (this.state.query !== '') {
+            furniture = furniture.filter(f => f.make.toLowerCase().includes(this.state.query.toLowerCase()) || f.model.toLowerCase().includes(this.state.query.toLowerCase()));
+        }
+        const pageLength = 2;
+        furniture = furniture.slice((page - 1) * pageLength, page * pageLength);
 
         return (
             <div className="container">
@@ -22,9 +48,16 @@ class HomePage extends Component {
                     <h1>Welcome to Furniture System</h1>
                     <p>Select furniture from the catalog to view details.</p>
 
-                    <form className="form-inline my-2 my-lg-0">
-                        <input className="form-control mr-sm-2" placeholder="Search" type="text" />
-                        <button className="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+                    <form className="form-inline my-2 my-lg-0" onSubmit={this.onSubmit}>
+                        <input
+                            onChange={this.onChange}
+                            value={this.state.query}
+                            name='query' 
+                            className="form-control mr-sm-2" 
+                            placeholder="Search" 
+                            type="text" 
+                        />
+                        <input className="btn btn-secondary my-2 my-sm-0" type="submit" value='Search' />
                     </form>
                 </div>
             </div>
@@ -44,7 +77,8 @@ function mapStateToprops (state){
 
 function mapDispatchToProps (dispatch) {
     return {
-        fetchPage: (page) => dispatch(fetchPageAction(page))
+        fetchPage: (page) => dispatch(fetchPageAction(page)),
+        fetchSerach: (query, page) => dispatch(fetchSearchAction(query, page))
     };
 }
     
