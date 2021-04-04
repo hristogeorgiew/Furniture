26d7a7d47d4d065f140116692181ef68d3623678
Hristo <36264387+hristogeorgiew@users.noreply.github.com>
2021-04-04 15:33:49 +0300

    Update Footer.js

--- client/src/components/Common/Footer.js
+++ client/src/components/Common/Footer.js
@@ -1,11 +1,70 @@
 
 const Footer = () => {
-    return(
+    return (
         <footer>
-        <div className="container modal-footer">
-            <p>Furniture System &copy; SoftUni 2021</p>
-        </div>
-    </footer>
+            <div className="container modal-footer">
+        
+                <div className="container p-4">
+                    <div className="row">
+                        <div className="col-lg-6 col-md-12 mb-4 mb-md-0">
+                            <h5 className="text-uppercase">Footer Content</h5>
+
+                            <p>
+                              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iste atque ea quis
+                              molestias. Fugiat pariatur maxime quis culpa corporis vitae repudiandae aliquam
+                              voluptatem veniam, est atque cumque eum delectus sint!
+                            </p>
+
+                        </div>
+
+                        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                              <h5 class="text-uppercase">Links</h5>
+
+                              <ul class="list-unstyled mb-0">
+                                <li>
+                                  <a href="#!" className="text-dark">Link 1</a>
+                                </li>
+                                <li>
+                                  <a href="#!" className="text-dark">Link 2</a>
+                                </li>
+                                <li>
+                                  <a href="#!" className="text-dark">Link 3</a>
+                                </li>
+                                <li>
+                                  <a href="#!" className="text-dark">Link 4</a>
+                                </li>
+                              </ul>
+                            </div>
+
+                            <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                                <h5 class="text-uppercase mb-0">Links</h5>
+
+                                <ul class="list-unstyled">
+                                  <li>
+                                    <a href="#!" className="text-dark">Link 1</a>
+                                  </li>
+                                  <li>
+                                    <a href="#!" className="text-dark">Link 2</a>
+                                  </li>
+                                  <li>
+                                    <a href="#!" className="text-dark">Link 3</a>
+                                  </li>
+                                  <li>
+                                    <a href="#!" className="text-dark">Link 4</a>
+                                  </li>
+                                </ul>
+                              </div>
+
+
+                    </div>
+                </div>
+            </div>
+            
+            <div className="text-center p-3" >
+                © 2021 Copyright:
+                <a className="text-dark" href="#">Hristo Georgiev</a>
+            </div>
+        </footer>
     );
 }
 
