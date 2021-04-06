445c4054b9d7b69822e9764928d7e5088e85d911
Hristo <h.georgiew93@gmail.com>
2021-04-06 16:15:32 +0300

    Add Header

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter } from 'react-router-dom';
 
 import RegisterPage from './components/Auth/RegisterPage';
 import LoginPage from './components/Auth/LoginPage';
 import HomePage from './components/Home/HomePage';
 import NotFound from './components/Common/NotFound';
 import CreatePage from './components/Create/CreatePage';
+import Header from './components/Common/Header';
 
 
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.onLogout = this.onLogout.bind(this);
+  }
+
+  onLogout() {
+    localStorage.clear();
+    this.props.history.push('/');
+  }
+
   render() {
     return (
       <div className='App'>
+        <Header loggedIn={false} onLogout={this.onLogout} />
         <main>
           <Switch>
             <Route exact path='/' component={HomePage} />
@@ -29,4 +43,4 @@ class App extends Component {
 }
 
 
-export default App;
+export default withRouter(App);
